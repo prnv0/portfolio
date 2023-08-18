e5dbb36400811937ded92db2d6e76fd9f60678ff
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-08-18 11:22:43 +0530

    updated about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,22 @@ function About() {
         <div className="aboutmain">
             <h1 className="aboutheading">About</h1>
             <div className="abouttext">
-            
+                <h3>I'm a third year Computer 
+                Science undergraduate
+                student with a passion for
+                technology. I have technical
+                experience in using
+                numerous programming
+                languages and frameworks for several projects but I commonly work on 
+                Front-End and Web Frameworks.
+                Fluent in English, Malayalam
+                and French with strong
+                communication skills and
+                technical writing ability.
+                </h3>
+            </div>
+            <div>
+                <button className="resume-button">Resume</button>
             </div>
         </div>
     );
