3a78ba6181ecf87880d193263899435581a8a956
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-08-20 12:12:52 +0530

    added nes.css library

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,14 +1,18 @@
 import "./styling.css";
+import "../../../node_modules/nes.css/css/nes.min.css"
 function Skills() {
     return (
         <div className="skillsmain">
             <h1 className="skillsheading">Skills</h1>
             <div className="skillstext">
-                <div className="skill">Python</div>
+                <div className="skill nes-container is-dark">Python</div>
                 <div className="skill">Java</div>
                 <div className="skill">Javascript</div>
                 <div className="skill">C</div>
-                <div className="skill"></div>
+                <div className="skill">ReactJS</div>
+                <div className="skill">Flask</div>
+                <div className="skill">MySQL</div>
+                <div className="skill">Google Cloud</div>
             </div>
         </div>
     );
