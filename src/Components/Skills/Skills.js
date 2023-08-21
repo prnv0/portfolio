f1ad8d09e629a0551b4908586b4773c30a4f5b75
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-08-21 16:31:55 +0530

    added icons

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,20 +1,24 @@
-import "./styling.css";
-import "../../../node_modules/nes.css/css/nes.min.css"
+import "./styling.css"
 function Skills() {
     return (
         <div className="skillsmain">
             <h1 className="skillsheading">Skills</h1>
             <div className="skillstext">
-                <div className="skill nes-container is-dark">Python</div>
-                <div className="skill">Java</div>
-                <div className="skill">Javascript</div>
-                <div className="skill">C</div>
-                <div className="skill">ReactJS</div>
-                <div className="skill">Flask</div>
-                <div className="skill">MySQL</div>
-                <div className="skill">Google Cloud</div>
+                <div className="skill div1"><div className="content"><img className="imgpython"></img><span>Python</span></div></div>
+                <div className="skill div2"><div className="content"><img className="imgjava"></img><span>Java</span></div></div>
+                <div className="skill div3"><div className="content"><img className="imgjavascript"></img><span>JavaScript</span></div></div>
+                <div className="skill div4"><div className="content"><img className="imgc"></img><span>C</span></div></div>
+                <div className="skill div5"><div className="content"><img className="imgreact"></img><span>ReactJS</span></div></div>
+                <div className="skill div6"><div className="content"><img className="imgflask"></img><span>Flask</span></div></div>
+                <div className="skill div7"><div className="content"><img className="imgmysql"></img><span>MySQL</span></div></div>
+                <div className="skill div8"><div className="content"><img className="imgdjango"></img><span>Django</span></div></div>
+                <div className="skill div9"></div>
+                <div className="skill div10"><div className="content"><img className="imggcp"></img><span>Google Cloud</span></div></div>
+                <div className="skill div11"></div>
+
             </div>
         </div>
+        
     );
 }
 export default Skills;
