07bcaee8143374c0ebc9ad06e30df3ca5ba8afef
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-09-01 21:56:15 +0530

    added project page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,9 +2,11 @@ import "./styling.css";
 function Projects() {
     return (
         <div className="projectsmain">
-            <h1 className="projectsheading">Projects</h1>
+            <div className="projectsheading"><span>Projects</span></div>
             <div className="projectstext">
-            
+                <div className="project project1"><img className="colorus" alt="project"></img><span> Colorus - your go-to color palette generator! Unleash creativity with random palettes or input a hex code for a personalized selection. Click to copy hexcodes of five captivating colors for your design endeavors. Elevate inspiration with Colorus - where hues meet innovation!</span></div>
+                <div className="project project1"><img className="colorus" alt="project"></img><span> Colorus - your go-to color palette generator! Unleash creativity with random palettes or input a hex code for a personalized selection. Click to copy hexcodes of five captivating colors for your design endeavors. Elevate inspiration with Colorus - where hues meet innovation!</span></div>
+                <div className="project project1"><img className="colorus" alt="project"></img><span> Colorus - your go-to color palette generator! Unleash creativity with random palettes or input a hex code for a personalized selection. Click to copy hexcodes of five captivating colors for your design endeavors. Elevate inspiration with Colorus - where hues meet innovation!</span></div>
             </div>
         </div>
     );
