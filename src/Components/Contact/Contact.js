bd79366a8b906819020e997e3e6e4b3b27c3aa20
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-09-02 15:43:24 +0530

    added contacts page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,13 @@
 import "./styling.css";
 function Contact() {
+    const socialClick = (str) => () => {
+        window.open(str);
+      };
     return (
         <div className="contactmain">
-        
+            <div className="contact" onClick={socialClick("mailto:pranavs13@gmail.com")}><img alt="" className="contactimg mail"></img></div>
+            <div className="contact" onClick={socialClick("https://www.linkedin.com/in/prnvv/")}><img alt="" className="contactimg linkedin"></img></div>
+            <div className="contact" onClick={socialClick("https://github.com/prnv0/")}><img alt="" className="contactimg github"></img></div>
         </div>
         
     );
